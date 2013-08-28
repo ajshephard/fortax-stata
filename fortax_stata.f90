8a5bda142a8449f5ff6d203535ced3e9c56d924e
Andrew Shephard <andrew.james.shephard@gmail.com>
2013-08-28 18:10:07 -0400

    updated licence text in fortax_stata

--- fortax_stata.f90
+++ fortax_stata.f90
@@ -1,21 +1,20 @@
-! This file is part of FORTAX for Stata.  
-! (c) 2009 Andrew Shephard; andrubuntu@gmail.com
-! 
+! This file is part of FORTAX for Stata.
+!
 ! FORTAX for Stata is free software: you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation, either version 3 of the License, or
 ! (at your option) any later version.
-! 
+!
 ! FORTAX for Stata is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-! 
+!
 ! You should have received a copy of the GNU General Public License
-! along with FORTAX for Stata.  If not, see <http://www.gnu.org/licenses/>. 
- 
+! along with FORTAX for Stata.  If not, see <http://www.gnu.org/licenses/>.
+
 module stata_save
- 
+
     use fortax_type, only : fam_t, sys_t, net_t
     use fortax_realtype, only : dp
 
